b21e185152d6368ab643afcc3c6061c8b45dee62
Matthew Flinders <sodaime@gmail.com>
2021-02-09 18:42:00 +0000

    upto exercises

--- learnopengl1/main.cpp
+++ learnopengl1/main.cpp
@@ -122,6 +122,19 @@ int main(int argc, char** argv)
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	/*
 	// Rectangle vertices split into triangles
 	unsigned int indices[] = {
@@ -264,9 +277,15 @@ int main(int argc, char** argv)
 		ourShader.setMat4("transform", &trans);
 		*/
 
-		model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime() * glm::radians(50.f),
-			glm::vec3(0.5f, 0.1f, 0.0f));
-		ourShader.setMat4("model", &model);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			model = glm::translate(glm::mat4(1.0f), cubePositions[i]);
+			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.f),
+				glm::vec3(0.5f, 0.1f, 0.0f));
+			ourShader.setMat4("model", &model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		ourShader.setFloat("mix_param", mix_param);
 
@@ -279,7 +298,7 @@ int main(int argc, char** argv)
 		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// Check and call events and swap buffers
 		glfwSwapBuffers(window);
