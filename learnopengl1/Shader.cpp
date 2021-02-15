5bf01a2f1fe3d4c20abd2d9f1e27c69dc10c7222
Matthew Flinders <sodaime@gmail.com>
2021-02-15 17:26:14 +0000

    add setVec3 which takes reference to vec3

--- learnopengl1/Shader.cpp
+++ learnopengl1/Shader.cpp
@@ -105,3 +105,8 @@ void Shader::setVec3(const std::string& name, float v0, float v1, float v2) cons
 	glUniform3f(glGetUniformLocation(ID, name.c_str()), v0, v1, v2);
 }
 
+void Shader::setVec3(const std::string& name, glm::vec3* value) const
+{
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(*value));
+}
+
