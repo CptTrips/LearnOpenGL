27d371b5f7bbfd32637ebef3596f7e23e568c42e
Matthew Flinders <sodaime@gmail.com>
2021-02-15 15:09:05 +0000

    add setVec3 routine for Shaders

--- learnopengl1/Shader.cpp
+++ learnopengl1/Shader.cpp
@@ -99,3 +99,9 @@ void Shader::setMat4(const std::string& name, glm::mat4* value) const
 {
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(*value));
 }
+
+void Shader::setVec3(const std::string& name, float v0, float v1, float v2) const
+{
+	glUniform3f(glGetUniformLocation(ID, name.c_str()), v0, v1, v2);
+}
+
